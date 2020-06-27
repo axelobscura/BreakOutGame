60c9c0bfda5d7197f6aeecefe493265a86b974ed
Axel Obscura <axosar@gmail.com>
2020-06-27 18:06:02 +0000

    Red square

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('el juego');
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(40,40,50,50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
