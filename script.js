59f5dcc6f0feec51cc8f4ca1a19826b5b3a02028
Axel Obscura <axosar@gmail.com>
2020-06-27 19:05:18 +0000

    bouncing and paddle

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
-
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+/*
 ctx.beginPath();
 ctx.rect(40,40,50,50);
 ctx.fillStyle = "#FF0000";
@@ -12,26 +14,78 @@ ctx.arc(200, 100, 10, 0, Math.PI*2, false);
 ctx.fillStyle = "#000";
 ctx.fill();
 ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 100);
-ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
-ctx.stroke();
-ctx.closePath();
+*/
 
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 1;
 var dy = -1;
+var ballRadius = 10;
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
+
+function square(){
+  ctx.beginPath();
+  ctx.rect(x, y, 100, 100);
+  ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#222222";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
 
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
-  ctx.fillStyle = "#333";
-  ctx.fill();
-  ctx.closePath();
-  x += dx;
-  y += dy;
+    drawBall();
+    square();
+    drawPaddle();
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
 };
-setInterval(draw, 10);
+setInterval(draw, 5);
